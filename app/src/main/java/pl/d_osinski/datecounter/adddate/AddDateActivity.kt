c7235e46b54861220c264d87d114688b5b9872c7
Daniel <osa9763@gmail.com>
2019-06-05 14:05:32 +0200

    s

---
+++ app/src/main/java/pl/d_osinski/datecounter/adddate/AddDateActivity.kt
@@ -0,0 +1,15 @@
+package pl.d_osinski.datecounter.adddate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.d_osinski.datecounter.R
+
+class AddDateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_date)
+
+        //TODO -> use this activity only to start fragment + viewmodel
+    }
+}
